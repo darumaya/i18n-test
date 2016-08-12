09ea93042123b1d702e12c2d93c72fb9d83e2d43
Ryosuke Otsuya <r.otsuya@gmail.com>
2016-08-12 19:41:55 +0900

    gruntが重いから軽くする
    一時的に画像の縮小処理を外す

--- Gruntfile.js
+++ Gruntfile.js
@@ -238,5 +238,5 @@ module.exports = function (grunt) {
     grunt.registerTask('dist-images', 'responsive_images');
 
     // Default task.
-    grunt.registerTask('default', ['dist-css', 'dist-images']);
+    grunt.registerTask('default', ['dist-css'/*, 'dist-images'*/]);
 };
